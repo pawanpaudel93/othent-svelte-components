d92e6cf19fdaaa5ae8de0c8bc4130014a4d707f1
Pawan Paudel <pawanpaudel93@gmail.com>
2023-05-25 19:28:36 +0545

    fix: rename key to store user data

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,4 +2,4 @@ import { persisted } from 'svelte-local-storage-store';
 import type { LogInReturnProps } from 'othent';
 import type { Writable } from 'svelte/store';
 
-export const userData: Writable<LogInReturnProps | null> = persisted('userData', null);
+export const userData: Writable<LogInReturnProps | null> = persisted('othent:userData', null);
